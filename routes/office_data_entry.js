91a6156dbf43d1a12a066b5bf8b380fc5e61cf02
khalid <khalidbinalik786@gmail.com>
2023-03-03 12:25:32 +0530

    ADD EDIT DELETE PURIFIER

--- routes/office_data_entry.js
+++ routes/office_data_entry.js
@@ -14,11 +14,27 @@ router.get("/wh_filter_add_edit_delete", function (req, res, next) {
   });
 });
 
+router.get("/purifier_add_edit_delete", function (req, res, next) {
+  office_data_entry_helpers.get_purifier_list().then((purifier_list) => {
+    res.render("./office_data_entry/purifier_add_edit_delete", {
+      purifier_list,
+    });
+  });
+});
+
 router.post("/add_wh_filter", function (req, res, next) {
   let wh_name = req.body.wh_filter.toUpperCase();
   office_data_entry_helpers.add_wh_filter_to_db(wh_name).then((ret_msg) => {
     console.log(ret_msg);
     res.redirect("wh_filter_add_edit_delete");
+  });
+});
+
+router.post("/add_purifier", function (req, res, next) {
+  let purifier_name = req.body.purifier.toUpperCase();
+  office_data_entry_helpers.add_purifier_to_db(purifier_name).then((ret_msg) => {
+    console.log(ret_msg);
+    res.redirect("purifier_add_edit_delete");
   });
 });
 
@@ -32,6 +48,18 @@ router.post("/edit_wh_filter", function (req, res, next) {
       res.redirect("wh_filter_add_edit_delete");
     });
 });
+
+router.post("/edit_purifier", function (req, res, next) {
+  let current_name = req.body.current_name;
+  let new_name = req.body.new_name.toUpperCase();
+  office_data_entry_helpers
+    .edit_purifier_to_db(current_name, new_name)
+    .then(() => {
+      console.log(current_name + "CHANGED TO " + new_name);
+      res.redirect("purifier_add_edit_delete");
+    });
+});
+
 router.post("/delete_wh_filter", function (req, res, next) {
   let wh_name = req.body.wh_filter;
   office_data_entry_helpers
@@ -39,6 +67,16 @@ router.post("/delete_wh_filter", function (req, res, next) {
     .then((ret_msg) => {
       console.log(ret_msg);
       res.redirect("wh_filter_add_edit_delete");
+    });
+});
+
+router.post("/delete_purifier", function (req, res, next) {
+  let purifier_name = req.body.purifier;
+  office_data_entry_helpers
+    .delete_purifier_from_db(purifier_name)
+    .then((ret_msg) => {
+      console.log(ret_msg);
+      res.redirect("purifier_add_edit_delete");
     });
 });
 
